29c2b52699520e3dce9b1e8bffaa9c72d203a007
ka123-web <cavi.vpm@gmail.com>
2021-02-06 12:10:55 -0500

    8. Styling Nav

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
 //import Pages
 import AboutUs from './Pages/AboutUs';
+import Nav from './components/Nav';
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutUs/>
     </div>
   );
--- src/components/FAQSection.js
+++ src/components/FAQSection.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import  styled from 'styled-components';
+import {About} from '../components/style';
 
 const FAQSection = () =>
 {
     return(
-        <div className="faq">
+        <Faq>
             <h2> Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How do i start</h4>
                 <div className="answer">
                     <p>this is a random text</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
 
             <div className="question">
@@ -17,6 +20,7 @@ const FAQSection = () =>
                 <div className="answer">
                     <p>this is a random text</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
 
             <div className="question">
@@ -24,6 +28,7 @@ const FAQSection = () =>
                 <div className="answer">
                     <p>this is a random text</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
 
             <div className="question">
@@ -31,9 +36,38 @@ const FAQSection = () =>
                 <div className="answer">
                     <p>this is a random text</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
 
     )
 }
+const Faq = styled(About)`
+display: block;
+span{
+    display:block;
+}
+h2{
+    padding-bottom: 2rem;
+    font-weight:lighter;
+}
+.faq-line{
+    background: #cccccc;
+    height: 0.2 rem;
+    margin:2rem 0 rem;
+    width:100%;
+
+}
+.question{
+    padding:3 rem 0rem;
+    cursor:pointer;
+}
+.answer{
+    padding: 2rem 0rem;
+    p{
+        padding:1 rem 0rem;
+    }
+}
+`;
+
 export default FAQSection;
