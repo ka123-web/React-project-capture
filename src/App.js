ede9200a2f505fafaef158f6361bc3ef1cc1d559
ka123-web <cavi.vpm@gmail.com>
2021-02-05 21:05:37 -0500

    5. Intro to styled Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+//Global Style
+import GlobalStyle from './components/GlobalStyle';
 //import Pages
 import AboutUs from './Pages/AboutUs';
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutUs/>
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,35 +1,69 @@
 import React from 'react';
 import home1 from '../Image/home1.png';
+//styled
+import styled from 'styled-components';
 
 
 const AboutSection =() =>
 {
     return (
-        <div>
-            <div className = "description">
+        <About>
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>we work to make</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>yout <span> dreams</span>come</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
+                        <h2>
+                            your <span> dreams  </span> come
+                        </h2>
+                    </Hide>
+                    <Hide>
                         <h2>True.</h2>
                         <p>Contact us for any photography or videography ideas you have. 
                             we have professionals with amazing skills to help to achieve it.
                         </p>
                         <button className="">Contact Us</button>
-                    </div>
+                    </Hide>
                 </div>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 
                 <img src={home1} alt="person with camera"/> 
-            </div>
-        </div>
+            </Image>
+        </About>
 
 
     )
 }
+
+//Styled Components
+const About = styled.div `
+min-height :90vh;
+display: flex;
+align-items:center;
+justify-content:space space-between;
+padding: 5rem 10 rem;
+color:white;
+`;
+const Description = styled.div`
+flex: 1;
+padding-right: 5 rem ;
+h2{
+    font-weight:lighter;
+}
+`;
+const Image = styled.div`
+flex:1;
+overflow:hidden;
+img{
+    width:100%;
+    height: 80vh;
+    object-fit:cover;
+}
+`;
+const Hide = styled.div `
+overflow:hidden;
+`
+
 export default AboutSection;
